50af7d1e27f5e76ab6137044cd72664e324d47f0
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-10-10 16:15:27 +0100

    add returns to prevent unnecessary executions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,43 @@
+const imageOne = "assets/chikorita.jpeg";
+const imageTwo = "assets/cyndaquil.png";
+const imageThree = "assets/totodile.png";
+
 const myButton = document.getElementById("my-button");
 const myParagraph = document.getElementById("my-paragraph");
 const characterImage = document.getElementById("character-img");
 
-let clickCounter = 0;
+let characterCounter = 1;
 
 function clickFunction() {
-  clickCounter = clickCounter + 1;
+  console.log("============== NEW TESTING STARTED ==============");
 
-  characterImage.src = "assets/cyndaquil.png";
-  myParagraph.innerHTML = "I have been clicked " + clickCounter + " time(s)";
+  characterCounter = characterCounter + 1;
+
+  if (characterCounter > 3) {
+    characterCounter = 1;
+  }
+
+  myParagraph.innerHTML = "I am on character " + characterCounter;
+
+  console.log("Testing if character counter is one");
+  if (characterCounter === 1) {
+    characterImage.src = imageOne;
+    return;
+  }
+
+  console.log("Testing if character counter is two");
+  if (characterCounter === 2) {
+    characterImage.src = imageTwo;
+    return;
+  }
+
+  console.log("Testing if character counter is three");
+  if (characterCounter === 3) {
+    characterImage.src = imageThree;
+    return;
+  }
+
+  console.log("No more testing to do");
 }
 
 myButton.onclick = clickFunction;
