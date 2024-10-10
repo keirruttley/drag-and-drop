6e7e00623b2c499e413e932cecd0af9a85ad4aaa
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-10-10 15:57:56 +0100

    change image upon button click + img style

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 const myButton = document.getElementById("my-button");
 const myParagraph = document.getElementById("my-paragraph");
+const characterImage = document.getElementById("character-img");
 
 let clickCounter = 0;
 
 function clickFunction() {
-  console.log("the button has been clicked");
   clickCounter = clickCounter + 1;
 
+  characterImage.src = "assets/cyndaquil.png";
   myParagraph.innerHTML = "I have been clicked " + clickCounter + " time(s)";
 }
 
