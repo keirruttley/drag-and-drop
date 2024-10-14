a6432118abab29a8ba3a7507b316e4d2dc3e6314
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-10-14 10:22:04 +0100

    remove drag / drop funcionalities

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -2,17 +2,10 @@ const draggable = document.getElementById("complement-one");
 const dropZone = document.getElementById("character-img");
 
 function onDrop(event) {
-  draggable.style.left = event.clientX + "px";
-  draggable.style.top = event.clientY + "px";
 }
 
 function onDragOver(event) {
-  event.preventDefault();
 }
 
 function onDragStart() {
 }
-
-draggable.ondragstart = onDragStart;
-dropZone.ondrop = onDrop;
-dropZone.ondragover = onDragOver;
