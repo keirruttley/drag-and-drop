cbf4a97fc509d1a978de30d9604418770123b4a1
keirruttley <Keir.Ruttley-Dornan@student.norwichuni.ac.uk>
2024-10-15 14:50:35 +0100

    on drop done

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -2,10 +2,14 @@ const energySword = document.getElementById("complement-one");
 const dropZone = document.getElementById ("character-img");
 
 function onDrop(event) { 
+    // bring energy sword to the postition (clientX clientY)
+    energySword.style.left = event.clientX + "px";
+    energySword.style.top = event.clientY + "px";
     console.log("Element has been dropped");
 }
 
 function onDragOver(event) {
+    event.preventDefault();
     console.log("Something is being dragged over me!");
 }
 
