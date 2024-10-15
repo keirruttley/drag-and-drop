7a5638201a1b19d7db3845a9e4e69daa07531eb3
keirruttley <Keir.Ruttley-Dornan@student.norwichuni.ac.uk>
2024-10-15 14:20:27 +0100

    dragandrop

---
+++ js/dragdrop.js
@@ -0,0 +1,18 @@
+const energySword = document.getElementById("complement-one");
+const dropZone = document.getElementById ("character-img");
+
+function onDrop(event) { 
+    console.log("Element has been dropped");
+}
+
+function onDragOver(event) {
+    console.log("Something is being dragged over me!");
+}
+
+function onDragStart() {
+    console.log ("I'm being dragged")
+}
+
+dropZone.ondrop = onDrop;
+dropZone.ondragover = onDragOver;
+fancyHat.ondragstart = onDragStart;
